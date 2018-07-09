2faa327ac222f4f81a3d20e8620a30a64608825b
ivan.barzakov <ivan.barzakov@delasport.com>
2018-07-09 17:13:37 +0300

    simple view implementation

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var Vues = new Vue({
+    el: ".obj",
+    data: {
+        "home_team":"Portland Trail Blazers",
+        "away_team":"Milwaukee Bucks",
+        "favorite":"home",
+        "order":"2017-12-01 03:05:00",
+        "event_state":"early",
+        "sport_key":"basketball",
+        "event_id":"1152133",
+        "event_info_line_one":"<span class=\"score row is-live\"> LIVE <\/span>",
+        "event_info_line_two":"01\/12 03:05",
+        "market_key":"fourteen_way_winning_margin",
+        "all_markets_button":"+37",
+        "red_cards_home":"0",
+        "red_cards_away":"0",
+        "has_live_incidents":false,
+        "terrain_type":"",
+        "home_team_color":"",
+        "away_team_color":"",
+        "live_clock_timestamp":"0",
+        "live_game_period":"''"
+    },
+    template:
+        ,
+});
